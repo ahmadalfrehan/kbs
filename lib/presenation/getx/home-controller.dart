c377e4fa514e212285195092af09b04bd08b0eb4
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-06-14 15:52:03 +0300

    message

--- lib/presenation/getx/home-controller.dart
+++ lib/presenation/getx/home-controller.dart
@@ -8,20 +8,57 @@ import '../../model/response-model.dart';
 
 class HomeController extends GetxController {
   RxString selected = 'select'.obs;
-  RxList<ResultModel> result = <ResultModel>[].obs;
-  RxBool isLoading  =  false.obs;
-  getData() async {
+
+  RxList<ResultModel> result1 = <ResultModel>[].obs;
+  RxList<ResultModel> result2 = <ResultModel>[].obs;
+  RxList<ResultModel> result3 = <ResultModel>[].obs;
+  RxList<List<String>> option = [
+    [
+      'vegetarian',
+      'vegan',
+    ],
+    [
+      'weight loss',
+      'muscle gain',
+    ],
+    [
+      'spicy',
+      'sweet',
+    ],
+  ].obs;
+  RxList messages = <String>[
+    'Enter your dietary requirement (vegetarian, vegan): ',
+    'Enter your health goal (weight loss, muscle gain):',
+    'Enter your personal preference (spicy, sweet): ',
+  ].obs;
+  RxList apiType = [
+    'types/',
+    'weight-muscle/',
+    'spicy-sweet/',
+  ].obs;
+  RxBool isLoading = false.obs;
+
+  getData(String apiType) async {
     isLoading.value = true;
+    print('http://127.0.0.1:8000/$apiType');
     try {
       final response = await http.post(
-          Uri.parse('http://127.0.0.1:8000/types/'),
+          Uri.parse('http://127.0.0.1:8000/$apiType'),
           body: json.encode({'input': selected.value}));
       log(response.body.toString());
       final data = json.decode(response.body);
-      result.clear();
+      result1.clear();
+      result2.clear();
+      result3.clear();
       Map<String, dynamic> userData = Map<String, dynamic>.from(data);
       for (var o in userData['message']) {
-        result.value.add(ResultModel.fromJson(o));
+        if (apiType == 'types/') {
+          result1.value.add(ResultModel.fromJson(o));
+        } else if (apiType == 'spicy-sweet/') {
+          result2.value.add(ResultModel.fromJson(o));
+        } else if (apiType == 'weight-muscle/') {
+          result3.value.add(ResultModel.fromJson(o));
+        }
       }
     } catch (error) {
       log(error.toString());
--- lib/presenation/home/home.dart
+++ lib/presenation/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kbs/presenation/getx/home-controller.dart';
+import 'package:kbs/presenation/home/options-results.dart';
 
 class Home extends GetView<HomeController> {
   const Home({Key? key}) : super(key: key);
@@ -26,36 +27,23 @@ class Home extends GetView<HomeController> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text(
-                          'Enter your dietary requirement (vegetarian, vegan): '),
-                      Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: DropdownButton<String>(
-                          isExpanded: true,
-                          hint: Text(controller.selected.value),
-                          items: <String>['vegetarian', 'vegan']
-                              .map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            controller.selected.value = value.toString();
-                            controller.getData();
+                      for (var i = 0; i < 3; ++i)
+                        OptionsResults(
+                          onPressed: (value) {
+                            print(value);
+                            controller.selected.value =value.toString();
+                            controller.getData(controller.apiType[i]);
+
                           },
+                          message: controller.messages[i],
+                          options: controller.option[i],
+                          results: i == 0
+                              ? controller.result1
+                              : i == 1
+                                  ? controller.result2
+                                  : controller.result3,
+                          selected: controller.selected.value,
                         ),
-                      ),
-                      const Text('Results:'),
-                      SizedBox(
-                        height: 100,
-                        child: ListView.builder(
-                            itemCount: controller.result.length,
-                            itemBuilder: (context, index) {
-                              return Text(
-                                  controller.result[index].result.toString());
-                            }),
-                      )
                     ],
                   ),
                 ),
