04383dd48112c8be7a5edd34953eae5289845284
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-06-14 15:23:20 +0300

    message

--- lib/presenation/getx/home-controller.dart
+++ lib/presenation/getx/home-controller.dart
@@ -9,20 +9,23 @@ import '../../model/response-model.dart';
 class HomeController extends GetxController {
   RxString selected = 'select'.obs;
   RxList<ResultModel> result = <ResultModel>[].obs;
-
+  RxBool isLoading  =  false.obs;
   getData() async {
+    isLoading.value = true;
     try {
       final response = await http.post(
           Uri.parse('http://127.0.0.1:8000/types/'),
-          body: json.encode({'input': selected}));
+          body: json.encode({'input': selected.value}));
       log(response.body.toString());
       final data = json.decode(response.body);
+      result.clear();
       Map<String, dynamic> userData = Map<String, dynamic>.from(data);
       for (var o in userData['message']) {
-        result.add(ResultModel.fromJson(o));
+        result.value.add(ResultModel.fromJson(o));
       }
     } catch (error) {
       log(error.toString());
     }
+    isLoading.value = false;
   }
 }
--- lib/presenation/home/home.dart
+++ lib/presenation/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kbs/presenation/getx/home-controller.dart';
 
-class Home extends StatelessWidget {
+class Home extends GetView<HomeController> {
   const Home({Key? key}) : super(key: key);
 
   @override
@@ -11,45 +13,53 @@ class Home extends StatelessWidget {
         title: const Text('Food assistant app'),
         backgroundColor: Colors.teal,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                  'Enter your dietary requirement (vegetarian, vegan): '),
-              Padding(
-                padding: const EdgeInsets.all(12),
-                child: DropdownButton<String>(
-                  isExpanded: true,
-                  hint: Text(selected),
-                  items: <String>['vegetarian', 'vegan'].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selected = value.toString();
-                      testApi();
-                    });
-                  },
+      body: Obx(
+        () => controller.isLoading.value
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.teal,
+                ),
+              )
+            : Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                          'Enter your dietary requirement (vegetarian, vegan): '),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: DropdownButton<String>(
+                          isExpanded: true,
+                          hint: Text(controller.selected.value),
+                          items: <String>['vegetarian', 'vegan']
+                              .map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            controller.selected.value = value.toString();
+                            controller.getData();
+                          },
+                        ),
+                      ),
+                      const Text('Results:'),
+                      SizedBox(
+                        height: 100,
+                        child: ListView.builder(
+                            itemCount: controller.result.length,
+                            itemBuilder: (context, index) {
+                              return Text(
+                                  controller.result[index].result.toString());
+                            }),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              const Text('Results:'),
-              SizedBox(
-                height: 100,
-                child: ListView.builder(
-                    itemCount: result.length,
-                    itemBuilder: (context, index) {
-                      return Text(result[index].result.toString());
-                    }),
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
